8f984f7277c3230486c02044907a3a2854713bce
Tusar Singh <rts0941@gmail.com>
2023-10-20 19:49:19 +0530

    Create a Web Project ' Radhey ' in Radhey.Web ASP DOT NET CORE WEB APP TARGET FRAMEWORK 8.0

---
+++ Radhey.Web/Radhey/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Radhey.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
